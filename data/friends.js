9aedaad1ae0a7b3b0940dc61f9c17538e4b6a0fd
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2019-10-24 12:47:23 -0700

    Pushing folder

---
+++ data/friends.js
@@ -0,0 +1,134 @@
+
+var friendsArray = [
+	{
+		name: 'Deadpool',
+		photo: 'https://miro.medium.com/max/3840/1*-4nkXQYN05ljzfJez_azbg.jpeg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'5',
+			'1',
+			'5',
+			'2',
+			'3',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Wolverine',
+		photo: 'https://static3.srcdn.com/wordpress/wp-content/uploads/2019/09/Wolverine-with-Metal-Claws-Comic.jpg',
+		scores: [
+			'3',
+			'2',
+			'5',
+			'3',
+			'2',
+			'2',
+			'4',
+			'4',
+			'5',
+			'4'
+		]
+	},
+	{
+		name: 'Hulk',
+		photo: 'https://partycity6.scene7.com/is/image/PartyCity/_pdp_sq_?$_1000x1000_$&$product=PartyCity/665292',
+		scores: [
+			'4',
+			'4',
+			'1',
+			'3',
+			'3',
+			'3',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Batman',
+		photo: 'https://upload.wikimedia.org/wikipedia/en/8/87/Batman_DC_Comics.png',
+		scores: [
+			'5',
+			'5',
+			'2',
+			'3',
+			'4',
+			'5',
+			'5',
+			'5',
+			'5',
+			'4'
+		]
+	},
+	{
+		name: 'Joker',
+		photo: 'https://vignette.wikia.nocookie.net/batmantheanimatedseries/images/c/c6/Joker_Card.jpg/revision/latest?cb=20160410014213',
+		scores: [
+			'4',
+			'1',
+			'2',
+			'5',
+			'1',
+			'5',
+			'2',
+			'1',
+			'1',
+			'2'
+		]
+	},
+	{
+		name: 'Harley Quinn',
+		photo: 'https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/harley-quinn-animated-series-e1511271802137.jpg',
+		scores: [
+			'2',
+			'1',
+			'2',
+			'5',
+			'2',
+			'2',
+			'2',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'The Flash',
+		photo: 'https://www.liveabout.com/thmb/35V7ht1J02e-g1VKdLDLvK4FIzE=/1384x900/filters:no_upscale():max_bytes(150000):strip_icc()/Flash_0001-58b5a1ba3df78cdcd87ed137.jpg',
+		scores: [
+			'5',
+			'5',
+			'4',
+			'4',
+			'4',
+			'5',
+			'2',
+			'5',
+			'1',
+			'1'
+		]
+	},
+	{
+	  "name": "Superman",
+	  "photo": "https://media.comicbook.com/2019/10/superman-18-reveals-clark-kent-secret-identity-to-world--1192193-1280x0.jpeg",
+	  "scores": [
+		"5",
+		"5",
+		"3",
+		"2",
+		"5",
+		"3",
+		"4",
+		"5",
+		"1",
+		"1"
+	  ]
+	}
+  
+];
+
+module.exports = friendsArray;
